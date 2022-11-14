cee5a77126cabda88d95712a75bf0e99aea6d1df
alwinponnanebslon <108860901+alwinponnanebslon@users.noreply.github.com>
2022-11-14 16:58:01 +0530

    deprecated issue

--- App.js
+++ App.js
@@ -1,117 +1,53 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import { NavigationContainer } from '@react-navigation/native';
+import axios from 'axios';
+import React, { createContext, useState } from 'react';
+import { SafeAreaView, Text } from 'react-native';
+import LoadingComponent from './src/Component/LoadingComponent';
+import ModalComponent from './src/Component/ModalComponent';
+import { RootProvider } from './src/Context/RootContext';
+import RootStack from './src/Navigation/stacks/RootStack';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+export const axiosApiInstance = axios.create();
+export const toggleModalContext = createContext();
+export const LoaderContext = createContext();
+export const modalValueContext = createContext();
+export const modalFunctionContext = createContext();
+// export const authContext = createContext();
+// export const kidContext = createContext();
+export const refContext = createContext();
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+export default function App() {
+  const [loading, setLoading] = useState(false);
+  const [toggleModal, setToggleModal] = useState(false);
+  const [message, setMessage] = useState('');
+  // const [isAuthorised, setIsAuthorised] = useState(false);
+  // const [currentKid, setCurrentKid] = useState({});
+  const [ref, setRef] = useState();
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+    <refContext.Provider value={[ref, setRef]}>
+      {/* <AnalyticsProvider client={segmentClient}> */}
+      <LoaderContext.Provider value={[loading, setLoading]}>
+        <toggleModalContext.Provider
+          value={{
+            toggleObj: [toggleModal, setToggleModal],
+            messageObj: [message, setMessage],
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+          <RootProvider>
+            <SafeAreaView style={{ flex: 1 }}>
+              <NavigationContainer>
+                <RootStack />
+              </NavigationContainer>
+            </SafeAreaView>
+          </RootProvider>
+          {toggleModal && <ModalComponent />}
+        </toggleModalContext.Provider>
+        {loading && <LoadingComponent />}
+      </LoaderContext.Provider>
+      {/* </AnalyticsProvider> */}
+    </refContext.Provider>
   );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+}
